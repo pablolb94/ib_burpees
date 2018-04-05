b0e48122682e93cb2677dc707cadf3d7707caed7
pablolb94 <38071259+pablolb94@users.noreply.github.com>
2018-04-05 12:39:47 +0200

    Add files via upload

--- sysfrm/config.php
+++ sysfrm/config.php
@@ -1,7 +1,1 @@
-<?php
-$db_host	    = 'localhost';
-$db_user        = 'jzpcnxpj_pablo';
-$db_password	= '}Q.myKS!X8dx';
-$db_name	    = 'jzpcnxpj_linares2';
-define('APP_URL', 'https://burpe.es/linares2');
-$_app_stage = 'Live'; // You can set this variable Live to Dev to enable ibilling Debug
+asd
